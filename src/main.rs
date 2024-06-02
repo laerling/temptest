692a418a2f2e594ba82585d0d0557f9962783421
laerling <laerling@posteo.de>
2024-06-02 18:33:14 +0200

    [rust] Ackermann

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,50 @@
-struct Foo {
-    a: i32,
-    b: *const i32,
-}
+use std::env;
 
-impl Foo {
-    fn new(val: i32) -> Self {
-	let mut foo = Foo {
-	    a: val, b: std::ptr::null(),
-	};
-	println!("{:8X}", (&foo as *const i32) as usize);
-	let self_ref: *const i32 = &foo.a;
-	foo.b = self_ref;
-	return foo
-    }
+fn print_stack(stack: &Vec<u64>) {
+    let stack_string: Vec<String> = stack.iter().map(|n| n.to_string()).collect();
+    println!("{}", stack_string.join(" "));
 }
-
-fn id<T: Unpin>(n: T) -> T { n }
 
 fn main() {
-    let a = Foo::new(13);
-    unsafe { println!("{}, {}", a.a, *a.b); }
-    let b = Foo::new(24);
-    unsafe { println!("{}, {}", b.a, *b.b); }
+
+    // parse args
+    let mut args = env::args().skip(1);
+    let arg1 = args.next().expect("Too few arguments");
+    let m: u64 = arg1.parse().unwrap_or_else(
+        |_| panic!("First argument cannot be parsed as number: {}", arg1));
+    let arg2 = args.next().expect("Too few arguments");
+    let n: u64 = arg2.parse().unwrap_or_else(
+        |_| panic!("Second argument cannot be parsed as number: {}", arg2));
+    println!("m={} n={}", m, n);
+
+    // do stuff
+    let mut stack = vec![m, n];
+    loop {
+
+        // print current iteration
+        print_stack(&stack);
+
+        // if only one element is left we're done
+        let len = stack.len();
+        if len == 1 {
+            break;
+        }
+
+        // else calculate Ackermann function
+        let m = stack[len-2];
+        let n = stack[len-1];
+        if m == 0 {
+            // A(0, n) = n+1
+            stack[len-1] += 1;
+            stack.remove(len-2);
+        } else if n == 0 {
+            // A(m, 0) = A(m-1, 1)
+            stack[len-2] -= 1;
+            stack[len-1] = 1;
+        } else {
+            // A(m, n) = A(m-1, A(m, n-1))
+            stack[len-1] -= 1;
+            stack.insert(len-2, m-1);
+        }
+    }
 }
