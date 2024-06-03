0fe3e8b7565d48f8a8f02c4781463f0225ce6fa1
laerling <laerling@posteo.de>
2024-06-03 14:33:58 +0200

    [rust] Ackermann: fixup! Track unresolved terms within the stack
    I will probably replace this with a separate list of terms that are
    either unresolved or resolved, where unresolved terms are associated
    with the index on the stack they're waiting for;
    
    stack    terms (hashmap)
    2 1      [2,1?0]
    1 2 0    [2,1?0 2,0?1]
    1 1 1    [2,1?0 2,0?1 1,1?1]
    1 0 1 0  [2,1?0 2,0?1 1,1?1 1,0?2]
    1 0 0 1  [2,1?0 2,0?1 1,1?1 1,0=2]
    1 0 2    [2,1?0 2,0=3 1,1=3 1,0=2 1,3?0]
    1 3      [2,1?0 2,0=3 1,1=3 1,0=2 1,3?0]
    0 1 2    [2,1?0 2,0=3 1,1=3 1,0=2 1,3?0 1,2?1]
    ...
    
    However, this approach has a drawback: While moving back
    up (i.e. shrinking) the stack we need to iterate through all values of
    the hashmap to see whether the index is in an unresolved term.
    
    More efficient would be to track unresolved terms not in the hashmap,
    but in a separate list, since we could simply use the stack index as
    lookup for that list.[1]
    
    So while growing the stack we'll look up (m,n) in the hashmap of
    resolved term, and while shrinking the stack we'll look up the stack
    index in the list of unresolved terms to see whether we can resolve any.
    
    stack    unresolved terms (list)          resolved terms (hashmap)
    2 1      [(2 1)]
    1 2 0    [(2 1) (2 0)]
    1 1 1    [[(2 1)] [(2 0) (1 1)]]
    1 0 1 0  [[(2 1)] [(2 0) (1 1)] [(1 0)]]
    1 0 0 1  [[(2 1)] [(2 0) (1 1)] [(1 0)]]
    1 0 2    [[(2 1)] [(2 0) (1 1)]]          [1,0=2]
    1 3      [[(2 1) (1 3)]]                  [1,0=2 2,0=3 1,1=3]
    0 1 2    [[(2 1) (1 3)] [(1 2)]]          [1,0=2 2,0=3 1,1=3]
    0 0 1 1  [[(2 1) (1 3)] [(1 2)]]          [1,0=2 2,0=3 1,1=3]
    0 0 3    [[(2 1) (1 3)] [(1 2)]]          [1,0=2 2,0=3 1,1=3]
    0 4      [[(2 1) (1 3)]]                  [1,0=2 2,0=3 1,1=3 1,2=4]
    5        []                               [1,0=2 2,0=3 1,1=3 1,2=4 2,1=5 1,3=5]
    
    [1] It's okay that the list of unresolved terms only tracks their stack
    index implicitly (i.e. via its own index into the list), because we
    can guarantee that no stack index will be skipped;
    - Either the term at a stack index is instantly resolvable (m==0) so
      we go back up the stack...
    - or it isn't, in which case it's either
      - a term that we know and thus can instantly resolve (going back up
        the stack)
      - or a term we have to add to the list of unresolved terms.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,68 @@
-use std::env;
+use std::collections::HashMap;
+use std::{env,fmt};
+use std::fmt::{Display,Formatter};
+use std::ops::{Add, Sub, AddAssign, SubAssign};
 
-fn print_stack(stack: &Vec<u64>) {
-    let stack_string: Vec<String> = stack.iter().map(|n| n.to_string()).collect();
+#[derive(Copy,Clone,PartialEq)]
+enum StackEntry {
+    Num(u64),
+    Term((u64,u64)),
+}
+
+impl Display for StackEntry {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            StackEntry::Num(n) => write!(f, "{}", n),
+            StackEntry::Term((m,n)) => write!(f, "({} {})", m, n),
+        }
+    }
+}
+
+impl Add for StackEntry {
+    type Output = Self;
+    fn add(self, other: Self) -> StackEntry {
+        match (self,other) {
+            (StackEntry::Num(a),
+             StackEntry::Num(b)) => StackEntry::Num(a+b),
+            _ => panic!("Can only add Num(_) with Num(_)"),
+        }
+    }
+}
+
+impl Sub for StackEntry {
+    type Output = Self;
+    fn sub(self, other: Self) -> StackEntry {
+        match (self,other) {
+            (StackEntry::Num(a),
+             StackEntry::Num(b)) => StackEntry::Num(a-b),
+            _ => panic!("Can only add Num(_) with Num(_)"),
+        }
+    }
+}
+
+impl AddAssign for StackEntry {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
+}
+
+impl SubAssign for StackEntry {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
+    }
+}
+
+fn print_stack(stack: &Vec<StackEntry>) {
+    let stack_string: Vec<String> = stack.iter()
+        .filter(|n_or_t| match **n_or_t {
+            StackEntry::Num(_) => true, _ => false
+        }).map(|n| n.to_string()).collect();
     println!("{}", stack_string.join(" "));
 }
 
 fn main() {
+
+    // TODO remember solutions to sub-problems and look them up
 
     // parse args
     let mut args = env::args().skip(1);
@@ -17,8 +74,10 @@ fn main() {
         |_| panic!("Second argument cannot be parsed as number: {}", arg2));
     println!("m={} n={}", m, n);
 
-    // do stuff
-    let mut stack = vec![m, n];
+    // prepare
+    let mut stack = vec![StackEntry::Num(m), StackEntry::Num(n)];
+    let solutions: HashMap<(u64,u64),u64> = HashMap::new();
+
     loop {
 
         // print current iteration
@@ -33,18 +92,18 @@ fn main() {
         // else calculate Ackermann function
         let m = stack[len-2];
         let n = stack[len-1];
-        if m == 0 {
+        if m == StackEntry::Num(0) {
             // A(0, n) = n+1
-            stack[len-1] += 1;
+            stack[len-1] += StackEntry::Num(1);
             stack.remove(len-2);
-        } else if n == 0 {
+        } else if n == StackEntry::Num(0) {
             // A(m, 0) = A(m-1, 1)
-            stack[len-2] -= 1;
-            stack[len-1] = 1;
+            stack[len-2] -= StackEntry::Num(1);
+            stack[len-1] = StackEntry::Num(1);
         } else {
             // A(m, n) = A(m-1, A(m, n-1))
-            stack[len-1] -= 1;
-            stack.insert(len-2, m-1);
+            stack[len-1] -= StackEntry::Num(1);
+            stack.insert(len-2, m-StackEntry::Num(1));
         }
     }
 }
