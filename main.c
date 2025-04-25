fdd858bbe241bd9674f98099b6671a39c9051761
laerling <laerling@posteo.de>
2025-04-25 18:26:35 +0200

    fixup! Test for descrete uniform distribution of N-bit values after sha256 'reroll'
    Let's say I want to deterministically choose a pseudo-random number
    between 1 and N (including) in a way that makes each number equally
    likely (this is called a descrete uniform distribution), but N is not
    a power of 2, so I cannot simply take the corresponding amount of bits
    from a random bit stream.
    
    I could take p bits where 2^(p-1)<N and 2^p>N and take the result
    modulo N, but then the first 2^p mod N numbers would have a higher
    probability than the others.
    
    Therefore I need to take p bits and reroll until the resulting encoded
    unsigned integer number is within 1 and N (including).
    
    This should make the probability of each number equal. But I want to
    test this empirically. This commit adds that empirical test for 1<=p<=8.

--- main.c
+++ main.c
@@ -2,25 +2,43 @@
 #include <string.h>
 #include "sha256.h"
 
-int main() {
+static unsigned int const AVG_HITS_PER_VAL = 256;
+
+void hash_str(char* str, BYTE* hash) {
 	// init hash
 	SHA256_CTX sha256_ctx;
 	for(int i=0;i<64;i++)
 		sha256_ctx.data[i] = '\0';
 	sha256_init(&sha256_ctx);
 
+        // for now we assume str to be shorter than 64
+        size_t str_len = strlen(str);
+
 	// update hash
 	BYTE data[64];
-	data[0] = 'h';
-	data[1] = 'i';
-	sha256_update(&sha256_ctx, (const BYTE*)data, 2);
+        strncpy((char*)data, str, str_len);
+	sha256_update(&sha256_ctx, (BYTE const*)data, 2);
 
 	// digest hash
-	BYTE hash[32];
 	sha256_final(&sha256_ctx, hash);
+}
 
-	// print hex hash
-	for(int i=0;i<32;i++)
-		printf("%02x", hash[i]);
-	printf("\n");
+int main() {
+        for(size_t bits=1; bits<=64; bits++) {
+                unsigned int const iterations = (1 << bits) * AVG_HITS_PER_VAL;
+                size_t const bytes_needed = (bits-1) / 8 + 1;
+                unsigned char* str = (unsigned char*)"deadbeef";
+                for(unsigned int i=0; i<iterations; i++) {
+
+                        // hash
+                        BYTE hash[32];
+                        // FIXME truncate string to amount of bytes
+                        hash_str(str, hash);
+
+                        // print hex hash
+                        for(int i=0;i<32;i++)
+                                printf("%02x", hash[i]);
+                        printf("\n");
+                }
+        }
 }
